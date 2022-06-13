7f5fb8ab34de3c342f81479770a5c02808b6d2b7
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-13 19:36:33 +0300

    Update 1.php

--- 00_arithmetic-operations/1.php
+++ 00_arithmetic-operations/1.php
@@ -4,4 +4,4 @@ function isFifteen(int $a, int $b): bool
     return $a == 15 || $b == 15 || $a + $b == 15 || $a - $b == 15;
 }
 
-var_dump(isFifteen(10,5)); // ar var_dump strada, bet ar echo nedod neko ara
+var_dump(isFifteen(10,5)); 
