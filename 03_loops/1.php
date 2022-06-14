6643b85c01cab855214d77fabcb5602a7631bb24
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-14 07:09:22 +0300

    Add files via upload

---
+++ 03_loops/1.php
@@ -0,0 +1,11 @@
+<?php
+
+$numbers = range(1,10);
+
+
+echo "The first 10 natural numbers are: " . implode(', ', $numbers) . PHP_EOL;
+
+
+
+
+//todo write a program that displays first 10 natural numbers
---
+++ 03_loops/2.php
@@ -0,0 +1,7 @@
+<?php
+
+$number = readline ('Enter number: ');
+echo $number ** $number . PHP_EOL
+
+
+//todo complete loop to multiply i with itself n times, it is NOT allowed to use built-in pow() function
