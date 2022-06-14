beec015bd1b095a868ebc43579c65cc1ee3018ee
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-14 07:15:38 +0300

    Update 1.php

--- 01_flow-of-control/1.php
+++ 01_flow-of-control/1.php
@@ -6,10 +6,4 @@ $putThirdNumber = readline("Input the 3rd number: " );
 
 echo "The max number was: " . max($putFirstNumber, $putSecondNumber, $putThirdNumber) . PHP_EOL;
 
-echo "Input the 1st number: " . $putFirstNumber . PHP_EOL;
-
-echo "Input the 2nd number: " . $putSecondNumber . PHP_EOL;
-
-echo "Input the 3rd number: " . $putThirdNumber . PHP_EOL;
-
 //todo print the largest number
