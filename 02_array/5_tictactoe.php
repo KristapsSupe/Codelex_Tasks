e9b7396ed84479a1fa9babfa32bd916fc82b74e1
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-17 09:39:24 +0300

    Add files via upload

--- 02_array/5_tictactoe.php
+++ 02_array/5_tictactoe.php
@@ -27,11 +27,11 @@ $board = [
 function displayBoard(array $board): void
 {
     echo "    0 | 1 | 2  \n";
-    echo "0   {$board[0][0]} | {$board[0][1]}  | {$board[0][2]}  \n";
+    echo "0   {$board[0][0]} | {$board[0][1]} | {$board[0][2]}  \n";
     echo "   ---+---+---\n";
-    echo "1   {$board[1][0]} | {$board[1][1]}  | {$board[1][2]}  \n";
+    echo "1   {$board[1][0]} | {$board[1][1]} | {$board[1][2]}  \n";
     echo "   ---+---+---\n";
-    echo "2   {$board[2][0]} | {$board[2][1]}  | {$board[2][2]}  \n";
+    echo "2   {$board[2][0]} | {$board[2][1]} | {$board[2][2]}  \n";
 }
 
 function hasWon(string $player, array $board): bool
