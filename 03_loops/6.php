6ec35eecd0ba166b15b5a58708fda271191fad05
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-25 08:34:56 +0300

    Add files via upload

--- 03_loops/6.php
+++ 03_loops/6.php
@@ -1,22 +1,17 @@
 <?php
-$left = '/';
-$middle = '*';
-$right = '\\';
 
-$leftCount = 16;
-$rightCount = 16;
-$middleCount = 0;
 
-for($i = 0; $i < 5; $i++)
+$height = 7;
+$width = ($height - 1) * 8;
+
+for($i = 0; $i < $height; $i++)
 {
-    echo str_repeat($left, $leftCount);
-    echo str_repeat($middle, $middleCount);
-    echo str_repeat($right, $rightCount);
+    echo str_repeat('/', $width / 2 - $i * 4);
+    echo str_repeat('*', $i * 8);
+    echo str_repeat('\\', $width / 2 - $i * 4);
 
     echo PHP_EOL;
 
-    $leftCount -= 4;
-    $middleCount += 8;
-    $rightCount -= 4;
+
 }
 
