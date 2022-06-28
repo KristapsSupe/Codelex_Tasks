27154de93f97f41588eec2407742f87dda7410e8
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-28 17:03:13 +0300

    Add files via upload

--- 03_loops/5.php
+++ 03_loops/5.php
@@ -1,27 +1,29 @@
 <?php
+
 echo "Welcome to Piglet" . PHP_EOL;
 
-$roll = true;
+$points = 0;
 
-while ($roll) {
+
+while (true) {
+
     $userInput = readline ('Roll the dice (y or n): ');
     $randomNumber = rand(1, 6);
-    $points = 0;
-    $pointsCount = $points + 2;
-
 
 
     switch ($userInput) {
 
         case 'y':
             echo 'You rolled ' . $randomNumber . PHP_EOL;
+            $points = $points + 2;
             break;
 
-        case 'n':
-            echo "You got " . $pointsCount . " points" . PHP_EOL;
-            exit;
 
+        case 'n':
+            echo $points . " points" . PHP_EOL;
+            exit;
     }
+
     if ($randomNumber === 1) {
         echo "You got 0 points"  . PHP_EOL;
         exit;
--- 03_loops/8.php
+++ 03_loops/8.php
@@ -1,12 +1,48 @@
 <?php
 
-$insertMin = (int)readline ('Min?: ') . PHP_EOL;
-$insertMax = (int)readline ('Max?: ') . PHP_EOL;
+$minNumber = (int)readline ('Min?: ');
+$maxNumber = (int)readline ('Max?: ');
 
-$range = range($insertMin, $insertMax);
+$numbers = range($minNumber, $maxNumber);
 
-foreach ($range as $rang) {
-    echo $rang;
+for ($i = $minNumber; $i <= $maxNumber; $i++)
+{
+    foreach ($numbers as $number)
+    {
+        echo $number . ' ';
+    }
+
+    $shiftedValue = array_shift($numbers);
+
+    $numbers[] = $shiftedValue;
+
+    echo PHP_EOL;
 }
 
 
+
+
+
+
+
+/**
+$left = '/';
+$middle = '*';
+$right = '\\';
+
+$leftCount = 16;
+$rightCount = 16;
+$middleCount = 0;
+
+for($i = 0; $i < 5; $i++)
+{
+echo str_repeat($left, $leftCount);
+echo str_repeat($middle, $middleCount);
+echo str_repeat($right, $rightCount);
+
+echo PHP_EOL;
+
+$leftCount -= 4;
+$middleCount += 8;
+$rightCount -= 4;
+}*/
