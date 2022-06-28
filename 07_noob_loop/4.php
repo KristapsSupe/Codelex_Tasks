7d5a9c98a88616e06f892ad6d1f4584d0dfa141a
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-28 04:50:22 +0300

    Add files via upload

--- 07_noob_loop/4.php
+++ 07_noob_loop/4.php
@@ -5,7 +5,6 @@ $userChar = readline('Choose char: ');
 
 for($i = 0; $i < $userNumber; $i++)
 {
-    echo str_repeat($userChar, $userNumber - $userNumber);
     echo str_repeat($userChar, $i + 1);
     echo PHP_EOL;
 }
