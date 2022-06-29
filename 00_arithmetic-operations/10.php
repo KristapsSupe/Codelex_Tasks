cb98b4c202cf7d334f57b3ffa52d9a10955b6e52
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-06-29 10:53:46 +0300

    Add files via upload

--- 00_arithmetic-operations/10.php
+++ 00_arithmetic-operations/10.php
@@ -7,28 +7,13 @@ echo "4. Quit\n";
 echo "Enter your choice (1-4) : ";*/
 
 
-$radius = 10;
 $pi = pi();
 
-$length = 50;
-$width = 10;
-
-$base = 20;
-$height = 30;
 
 
-    function circle(int $pi, int $radius): int
-    {
-        return $pi * $radius * 2;
-    }
-if ($radius < 0)
+function circle(int $pi, int $radius): int
 {
-echo "Negative value" . PHP_EOL;
-}
-
-else
-{
-    echo circle($pi, $radius) . PHP_EOL;
+    return $pi * $radius * 2;
 }
 
 
@@ -37,22 +22,61 @@ function rectangle(int $length, int $width): int
     return $length * $width;
 }
 
-if ($length < 0 || $width < 0) {
-    echo "Negative value" . PHP_EOL;
-} else {
-    echo rectangle($length, $width) . PHP_EOL;
-}
-
 
 function triangle(int $base, int $height): int
 {
     return $base * $height * 0.5;
 }
 
-if ($base < 0 || $height < 0) {
-    echo "Negative value" . PHP_EOL;
-} else {
-    echo triangle($base, $height) . PHP_EOL;
+
+echo 'Geometry Calculator: ' . PHP_EOL;
+echo PHP_EOL;
+echo '1 - Calculate the Area of a Circle: ' . PHP_EOL;
+echo '2 - Calculate the Area of a Rectangle: ' . PHP_EOL;
+echo '3 - Calculate the Area of a Triangle: ' . PHP_EOL;
+echo '4 - Quit: ' . PHP_EOL;
+
+$selection = readline('Enter your choice (1-4): ');
+
+switch ($selection) {
+    case 1:
+        $radius = readline('Enter radius: ');
+        if ($radius < 0)
+        {
+            echo "Negative value" . PHP_EOL;
+        }
+        {
+            echo circle($pi, $radius) . PHP_EOL;
+        }
+        break;
+
+    case 2:
+        $length = readline('Enter length: ');
+        $width = readline('Enter width: ');
+        if ($length < 0 || $width < 0)
+        {
+            echo "Negative value" . PHP_EOL;
+        }
+        {
+            echo rectangle($length, $width) . PHP_EOL;
+        }
+        break;
+
+    case 3:
+        $base = readline('Enter length: ');
+        $height = readline('Enter width: ');
+        if ($base < 0 || $height < 0)
+        {
+            echo "Negative value" . PHP_EOL;
+        }
+        {
+            echo triangle($base, $height) . PHP_EOL;
+        }
+        break;
+
+    default:
+        echo "Bye!";
+        break;
+
 }
 
-// jauztaisa klase
