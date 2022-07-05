fb20318f7f09827ccb965cf61792e9a57c8a8648
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-07-05 18:05:04 +0300

    Add files via upload

--- 06_noob_array/21.php
+++ 06_noob_array/21.php
@@ -1,6 +1,6 @@
 <?php
-$person = [
-    ["name" => "Bob", "age" => 50, "voted" => true],
+$persons = [
+    ["name" => "Bob", "age" => 30, "voted" => true],
     ["name" => "Jake", "age" => 32, "voted" => true],
     ["name" => "Kate", "age" => 25, "voted" => false],
     ["name" => "Sam", "age" => 20, "voted" => false],
@@ -14,11 +14,38 @@ $person = [
     ["name" => "Zack", "age" => 19, "voted" => false],
 ];
 
-
-$filter = "voted";
-
-$voted = array_filter($person, function($var) use ($filter){
-    return ($var['voted'] == $filter);
+$youngVotersTotal = array_filter($persons, function($x) {
+    return  $x["age"] <= 25;
+});
+$youngVotersVoted = array_filter($persons, function($x) {
+    return  $x["age"] <= 25 && $x["voted"] === true;
 });
 
-echo count($voted);
+
+$midVotersTotal = array_filter($persons, function($x) {
+    return  $x["age"] >= 26 && $x["age"] <= 35;
+});
+$midVotersVoted = array_filter($persons, function($x) {
+    return  $x["age"] >= 26 && $x["age"] <= 35 && $x["voted"] === true;
+});
+
+
+$oldVotersTotal = array_filter($persons, function($x) {
+    return  $x["age"] >= 36;
+});
+$oldVotersVoted = array_filter($persons, function($x) {
+    return  $x["age"] >= 36 && $x["voted"] === true;
+});
+
+echo "Young Voters Total: "  . count($youngVotersTotal) . PHP_EOL;
+echo "Young Voters Voted: "  . count($youngVotersVoted) . PHP_EOL;
+
+echo "Mid Voters Total: "  . count($midVotersTotal) . PHP_EOL;
+echo "Mid Voters Voted: "  . count($midVotersVoted) . PHP_EOL;
+
+echo "Old Voters Total: "  . count($oldVotersTotal) . PHP_EOL;
+echo "Old Voters Voted: "  . count($oldVotersVoted) . PHP_EOL;
+
+//var_dump($youngVotersVoted);
+//var_dump($midVotersVoted);
+//var_dump($oldVotersVoted);
