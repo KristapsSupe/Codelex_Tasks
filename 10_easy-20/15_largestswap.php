978f0e97a7746e713285618ee210c2991e116fa9
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-07-11 12:38:08 +0300

    Add files via upload

--- 10_easy-20/15_largestswap.php
+++ 10_easy-20/15_largestswap.php
@@ -1,5 +1,25 @@
 <?php
+/**
+ * Create a function that takes an array of numbers and return both the minimum and maximum numbers,
+ * in that order in a new array.
+ */
+
+
+class LargestSwap
+{
+    public static function swapTheLargest(int $number): string
+    {
+        if ($number > strrev($number)) {
+            echo "True";
+        } else {
+            echo "False";
+        }
+        return "";
+    }
+}
 
 
 
-
+echo LargestSwap:: swapTheLargest(27) . PHP_EOL;
+echo LargestSwap:: swapTheLargest(43) . PHP_EOL;
+echo LargestSwap:: swapTheLargest(14) . PHP_EOL;
