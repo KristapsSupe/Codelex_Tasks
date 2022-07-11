2e56833ca3daa5049d6a097e7ad44d73d8db1848
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-07-11 11:30:19 +0300

    Add files via upload

--- 10_easy-20/11_kindergarten.php
+++ 10_easy-20/11_kindergarten.php
@@ -5,31 +5,45 @@
  * Write a function that reverses all the words in a sentence that start with a particular letter.
  */
 
-$sentence = "thank you for teaching us programming!";
-
-$splittedSentences = explode(' ', $sentence);
-
-
-if (array_filter($splittedSentences,)) {
-    echo strrev($sentence);
-};
-
-echo $position = strpos($sentence, 't');
-
-
-
-//class Kindergarten
-//{
+//$sentence = "First man to walk on the moon";
+//$letter = "m";
 //
-//    public static function translate(string $sentence): string
-//    {
-//        if (substr($sentence, 0, 1) === "t") {
-//            $reversedSentence = strrev($sentence);
-//        }
-//        return $reversedSentence;
+//$splittedSentences = explode(' ', $sentence);
+//
+//foreach ($splittedSentences as $word) {
+//    if (substr($word, 0, 1) === $letter)
+//        echo strrev($word) . " ";
+//    else {
+//        echo $word  . " ";
 //    }
 //}
-//
-//$sentence = "Thank you for teaching us programming!";
-//
-//echo Kindergarten:: translate($sentence);
+
+
+
+
+
+
+
+
+class Kindergarten
+{
+
+    public static function translate(string $sentence, string $letter): string
+    {
+        $splittedSentences = explode(' ', $sentence);
+
+        foreach ($splittedSentences as $word) {
+            if (substr($word, 0, 1) === $letter)
+                echo strrev($word) . " ";
+            else {
+                echo $word  . " ";
+            }
+        }
+        return "";
+    }
+
+}
+
+echo Kindergarten:: translate("First man to walk on the moon", "m" ) . PHP_EOL;
+echo Kindergarten:: translate("Peter ate pepper", "p" ) . PHP_EOL;
+echo Kindergarten:: translate("I picked pickled onions for supper", "p" ) . PHP_EOL;
