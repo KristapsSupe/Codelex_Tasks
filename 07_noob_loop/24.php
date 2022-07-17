6e2bab0bc7bbfaa66502cceb3c8e8a49e8bfab08
Kristaps Supe <103953359+KristapsSupe@users.noreply.github.com>
2022-07-17 20:56:57 +0300

    Add files via upload

---
+++ 07_noob_loop/24.php
@@ -0,0 +1,47 @@
+<?php
+$n = readline("Enter number: ");
+
+$centerLines = $n;
+
+echo str_repeat('.', $n / 2);
+echo str_repeat('#', $centerLines);
+echo str_repeat('.', $n / 2);
+echo PHP_EOL;
+
+for($i = 1; $i <= $n - 2; $i++)
+{
+    echo str_repeat('.', $n / 2);
+    echo str_repeat('#', 1);
+    echo str_repeat('.', $n - 2);
+    echo str_repeat('#', 1);
+    echo str_repeat('.', $n / 2);
+    echo PHP_EOL;
+}
+
+echo str_repeat('#', $n / 2 + 1);
+echo str_repeat('.', $n - 2);
+echo str_repeat('#', $n / 2 + 1);
+echo PHP_EOL;
+
+for($i = 2; $i <= $n; $i++) {
+    for ($j = 1; $j < $i; $j++) {
+        echo ".";
+    }
+    for ($j = $i; $j < $n; $j++) {
+        if($j == $i)
+            echo "#";
+        else
+            echo ".";
+    }
+    for($j = $i; $j <= $n;  $j++) {
+        if ($j == $n)
+            echo "#";
+        else
+            echo ".";
+    }
+    for ($j = 1; $j < $i; $j++) {
+        echo ".";
+    }
+
+    echo PHP_EOL;
+}
